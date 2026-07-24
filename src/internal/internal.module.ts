5709d0fe60c3d5c999d20dad7f5901504860e07b
Illia Kyselov <doncerber@gmail.com>
2026-07-24 18:06:04 +0300

    feat(api): gate /login to admin/superadmin (not_admin for teachers)

--- src/internal/internal.module.ts
+++ src/internal/internal.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AgentLogModule } from '../agent-log/agent-log.module';
 import { AnnouncementsModule } from '../announcements/announcements.module';
+import { RbacModule } from '../rbac/rbac.module';
 import { ScheduleModule } from '../schedule/schedule.module';
 import { SubstitutionService } from '../substitution/substitution.service';
 import { InternalAuthController } from './internal-auth.controller';
@@ -9,7 +10,7 @@ import { InternalTokenGuard } from './internal-token.guard';
 import { InternalService } from './internal.service';
 
 @Module({
-  imports: [ScheduleModule, AnnouncementsModule, AgentLogModule],
+  imports: [ScheduleModule, AnnouncementsModule, AgentLogModule, RbacModule],
   controllers: [InternalController, InternalAuthController],
   providers: [InternalService, InternalTokenGuard, SubstitutionService],
 })
